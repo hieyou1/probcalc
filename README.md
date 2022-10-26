4f3dc969321e3ff04702f7e935c9aa655d97d761
hieyou1 <31020711+hieyou1@users.noreply.github.com>
2022-10-25 20:49:39 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# probcalc
+
+Yep, sometimes professors/teachers can give you homework problems from a textbook which require you to do math before the actual homework. This attempts to alleviate that problem.
+
+## Supported Types
+
+- eoo: Every other odd (this one's a fun one)
+- odds
+- (pick 2): Picks the odds every time
+- evens
+- all
+- (Single problems): Just write the number and you're good!
+
+Instructions are also available by clicking "Show Instructions".
---
+++ main.js
@@ -0,0 +1,76 @@
+const DOM = {
+    "problems": document.getElementById("problems"),
+    "submit": document.getElementById("calc"),
+    "inst": document.getElementById("instructions"),
+    "toggle": document.getElementById("showinst")
+};
+
+DOM.toggle.onclick = () => {
+    if (DOM.inst.style.display == "none") {
+        DOM.inst.style.display = "";
+        DOM.toggle.innerText = "Hide instructions";
+    } else {
+        DOM.inst.style.display = "none";
+        DOM.toggle.innerText = "Show instructions";
+    }
+};
+
+DOM.submit.onclick = () => {
+    DOM.submit.disabled = true;
+    let problems = DOM.problems.value.toLowerCase().trim().split(", "); // [ "5-61 eoo", "62", "67" ]
+    let totalProblems = 0;
+    let probStr = "";
+    let addProblem = (num) => {
+        ++totalProblems;
+        if (probStr.length == 0) probStr += num; else probStr += ", " + num;
+    };
+    for (let i of problems) {
+        if (i.indexOf("-") == -1) addProblem(i); else {
+            let start = parseInt(i.split("-")[0]);
+            let end = parseInt(i.split("-")[1].split(" ")[0]);
+            if (isNaN(start) || isNaN(end)) {
+                window.alert("Unknown number: " + (isNaN(start) ? start : end));
+                window.location.reload();
+                return;
+            }
+            let operator = i.split(" ")[1] ?? "all";
+            switch (operator) {
+                case "eoo": {
+                    let add = true;
+                    for (let i = start; i <= end; ++i) {
+                        if (i % 2 != 0) {
+                            if (add) addProblem(i);
+                            add = !add;
+                        }
+                    }
+                    break;
+                }
+                case "odds":
+                case "(pick": {
+                    for (let i = start; i <= end; ++i) {
+                        if (i % 2 != 0) addProblem(i);
+                    }
+                }
+                case "evens": {
+                    for (let i = start; i <= end; ++i) {
+                        if (i % 2 == 0) addProblem(i);
+                    }
+                    break;
+                }
+                case "all": {
+                    for (let i = start; i <= end; ++i) addProblem(i);
+                    break;
+                }
+                default: {
+                    window.alert("Unknown operator: " + operator);
+                    window.location.reload();
+                    return;
+                }
+            }
+        }
+    }
+    document.getElementById("card-body").innerHTML = `<b>Total: ${totalProblems}</b><br /><br />Problems: ${probStr}`;
+};
+window.onload = () => {
+    DOM.submit.disabled = false;
+};
